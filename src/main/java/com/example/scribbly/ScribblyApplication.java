74d275c197b55100a62ba408a74deb560546c877
parksh <icecream080118@gmail.com>
2025-04-23 13:54:23 +0900

    First commit

---
+++ src/main/java/com/example/scribbly/ScribblyApplication.java
@@ -0,0 +1,13 @@
+package com.example.scribbly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribblyApplication.class, args);
+	}
+
+}
